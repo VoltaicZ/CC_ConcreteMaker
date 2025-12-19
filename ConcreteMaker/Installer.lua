fe09033d497654c7c2e6cd6ee6d64e94ec2ef00f
Zeth <patscripts@gmail.com>
2025-12-19 14:51:58 -0500

    Updated Installer again

--- ConcreteMaker/Installer.lua
+++ ConcreteMaker/Installer.lua
@@ -1,4 +1,7 @@
 fs.makeDir("CC_ConcreteMaker")
 
+print("Downloading Files...")
 shell.run("wget https://raw.githubusercontent.com/VoltaicZ/CC_ConcreteMaker/refs/heads/main/ConcreteMaker/ConcreteManager.lua CC_ConcreteMaker/ConcreteManager.lua")
+print("Downloaded ConcreteManager.lua...")
 shell.run("wget https://raw.githubusercontent.com/VoltaicZ/CC_ConcreteMaker/refs/heads/main/ConcreteMaker/ButtonManager.lua CC_ConcreteMaker/ButtonManager.lua")
+print("Downloaded ButtonManager.lua...")
