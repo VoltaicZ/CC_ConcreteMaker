cf4609508fb850c3c3f66e7018e549948ec33274
Zeth <patscripts@gmail.com>
2025-12-19 15:28:18 -0500

    trying

--- ConcreteMaker/ConcreteManager.lua
+++ ConcreteMaker/ConcreteManager.lua
@@ -4,4 +4,4 @@ local ButtonManager = require("ButtonManager")
 --//Vars
 
 ButtonManager.Init("back")
-ButtonManager.CreateButton({1,1}, {10,10}, true, "blue")
+ButtonManager.CreateButton({1,1}, {10,10}, true, "blue", "Hello World")
