6d3d1b7052f55bf5f87af151a003dc1e5b901107
Zeth <patscripts@gmail.com>
2025-12-19 14:57:46 -0500

    Fixed spelling error in manager

--- ConcreteMaker/ConcreteManager.lua
+++ ConcreteMaker/ConcreteManager.lua
@@ -4,4 +4,4 @@ local ButtonManager = require("ButtonManager")
 --//Vars
 local Monitor = peripheral.find("monitor")
 
-Monitor.CreateButton({0,0}, {5,5}, true, "blue")
+ButtonManager.CreateButton({0,0}, {5,5}, true, "blue")
