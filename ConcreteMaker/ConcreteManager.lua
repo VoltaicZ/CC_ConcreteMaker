4f7fc3b0027426901bfcf95453f04e2b513a0757
Zeth <patscripts@gmail.com>
2025-12-19 17:11:14 -0500

    better size test

--- ConcreteMaker/ConcreteManager.lua
+++ ConcreteMaker/ConcreteManager.lua
@@ -4,16 +4,10 @@ local ButtonManager = require("ButtonManager")
 --//Vars
 local Monitor = peripheral.wrap("back")
 local Width, Height = Monitor.getSize()
-local ButtonWidth = 6
-local ButtonHeight = 2
 
-local ActualDimenstions = {
-    Width-ButtonWidth+1,
-    Height-ButtonHeight+1
-}
 
 ButtonManager.Init("back")
-ButtonManager.CreateButton({1,1}, ActualDimenstions, true, "blue", "Hello World", 
+ButtonManager.CreateButton({1,1}, {6,1}, true, "blue", "Hello World", 
 function() 
     print("Button Pressed") 
 end)
